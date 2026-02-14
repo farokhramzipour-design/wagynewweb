74c3c773db2d49d9209bd417a94947644babdf04
behnam <behnam@behnams-MacBook-Pro.local>
2026-02-14 14:21:28 -0800

    first commit

--- lib/dates.ts
+++ lib/dates.ts
@@ -1,10 +1,7 @@
 import dayjs from "dayjs";
-import jalaliday from "jalaliday";
-
-dayjs.extend(jalaliday);
 
 export function formatDate(date?: string, locale: "fa" | "en" = "fa") {
   if (!date) return "";
-  const instance = locale === "fa" ? dayjs(date).calendar("jalali") : dayjs(date);
+  const instance = dayjs(date);
   return instance.format(locale === "fa" ? "YYYY/MM/DD" : "YYYY-MM-DD");
 }
